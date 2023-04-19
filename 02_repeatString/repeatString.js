497129822199b8733272275ef66921d46360fdf8
AdleenaLobo <111069455+AdleenaLobo@users.noreply.github.com>
2023-04-19 09:36:22 +0530

    complete repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(b, a) {
+let c="";
+if (a<0)
+{
+	return 'ERROR';
+}
+//here b==0 or b=='' or b==null works just fine 
+if (b==null)
+	{
+		return '';
+	}
+for (i=0; i<a;i++)
+	{
+	c+=b;
+	}
+	return c;
 };
 
 // Do not edit below this line
