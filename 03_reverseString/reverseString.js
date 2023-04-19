e9077aada57e407a9b2a747f03d2e8d7ed8fedfa
AdleenaLobo <111069455+AdleenaLobo@users.noreply.github.com>
2023-04-19 10:25:51 +0530

    Complete reverse strings.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(a) {
+let b='';
+let c= a.length;
+for (i=c-1;i>=0;i--)
+	{
+		b+=a.charAt(i);
+	}
+	return b;
 };
 
 // Do not edit below this line
