d1b2eabb30177056df0bdba2fd1e79a4ec78d958
AdleenaLobo <111069455+AdleenaLobo@users.noreply.github.com>
2023-04-20 10:01:57 +0530

    Edit

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(a,...b) {
+	let counter=0;
+	for (i=0;i<a.length;)
+		{
+					for(const d of b)
+				{
+					if(a[i]===d)
+						{
+							a.splice(i,1);
+							counter=1;
+						}
+						else
+							{counter=0;}
+				}
+				if (counter==0)
+					{
+						i++;
+					}
+		}
+return a;
 };
 
 // Do not edit below this line
