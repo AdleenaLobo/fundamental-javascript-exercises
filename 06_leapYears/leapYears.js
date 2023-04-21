fe756ed10ca43051f5d783f325200eabccf642c9
AdleenaLobo <111069455+AdleenaLobo@users.noreply.github.com>
2023-04-21 10:06:20 +0530

    Complete Leap Years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,24 @@
-const leapYears = function() {
+const leapYears = function(yearNumber) {
+    if(yearNumber%4==0)
+    {
+        if(yearNumber%100==0 && yearNumber%400==0)
+        {
+            return true;
+        }
+        else if (yearNumber%100==0 && yearNumber%400!=0)
+        {
+            return false;
+        }
+        else
+        {return true;}
+    }
+    else
+    {
+        return false;
+    }
 
 };
+
 
 // Do not edit below this line
 module.exports = leapYears;
