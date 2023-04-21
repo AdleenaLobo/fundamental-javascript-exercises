58da8119d4d02300aca4b0cf3c9b8dd6ae2d952f
AdleenaLobo <111069455+AdleenaLobo@users.noreply.github.com>
2023-04-21 09:44:59 +0530

    Complete sum of all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+	let sum=0;
+	let c=(a<b)? a:b;
+	let d=(a<b)? b:a;
+	if ((Number.isInteger(a) && Number.isInteger(b))&& (a>=0 &&b>=0)){
+	for(let i=c;i<=d;i++)
+		{
+			sum=sum+i;
+		}
+		return sum;}
+	else
+	{
+	    return "ERROR"
+	}
 
 };
+console.log(sumAll(123, 1));
+
 
 // Do not edit below this line
 module.exports = sumAll;
